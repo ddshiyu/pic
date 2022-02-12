1838db295a5ad767720ff47ca67b241bdb503efc
建东 <ddshiyu@163.com>
2022-02-12 21:02:28 +0800

    boomb(create): tongji.js

---
+++ tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7f5ae45b9e1e32841ef570235ab95024";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
